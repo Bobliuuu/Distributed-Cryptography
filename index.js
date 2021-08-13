a079310105192c73948e3c71f0d1341c91a56586
Jerry <54565411+Bobliuuu@users.noreply.github.com>
2021-08-13 13:30:43 -0400

    Add base site

---
+++ index.js
@@ -0,0 +1,9 @@
+function bob() {
+	window.location.href = "bob/bob.html";
+}
+function alice() {
+	window.location.href = "alice/alice.html";
+}
+function eve() {
+	window.location.href = "eve/eve.html";
+}
